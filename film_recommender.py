73fb91d78e6c66de2f2dc1d5517917a81a1da5bf
kleax <75224022+kleax@users.noreply.github.com>
2025-06-02 18:11:03 +0300

    Update film_recommender.py

--- film_recommender.py
+++ film_recommender.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
-
+st.write("✅ Kod başladı.")
 # -------------------- VERİ YÜKLEME --------------------
 @st.cache_data
 def load_data():
